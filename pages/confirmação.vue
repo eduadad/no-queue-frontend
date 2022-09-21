85c0bc493e46acd44fd6eedefd901c27703f1457
enzoscorteganha <111440538+enzoscorteganha@users.noreply.github.com>
2022-09-21 08:25:00 -0300

    Create confirmação.vue

---
+++ pages/confirmação.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+
+  
+  <div class="columns">
+
+    
+      <div class="column is-half mb-1">
+        <br>
+
+        <h3 class="title is-3"><strong>Parabéns, sua reserva foi confirmada!</strong></h3>
+        <br> O número da sua reserva é: <strong>2017887342</strong>
+        <br> Ao chegar no seu destino, apresente este <strong>QR CODE</strong> abaixo
+        <br> <strong>DATA:</strong> 07/10/2022
+        <br> <strong>HORÁRIO:</strong> Das 14:00 até as 17:00
+      
+
+       
+         <b-image style = 'width: 300px;' class="image mt-2" is-6 :src="require('@/assets/qrcolde.png')"></b-image>
+       
+      </div>
+    
